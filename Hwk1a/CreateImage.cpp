f43b33839e083e03b4290fe28e67a2a4b744f4be
bmatern-nmdp <bmatern@L0504206.local>
2015-09-22 23:06:40 -0500

    it works now
    just needs some cleanup

--- Hwk1a/CreateImage.cpp
+++ Hwk1a/CreateImage.cpp
@@ -20,7 +20,7 @@ using namespace std;
 //It's not the same as a c++ vector.  Or a virology vector.  How confusing.
 typedef struct
 {
-	float x,y,z;
+	double x,y,z;
 } PointType;
 
 typedef struct 
@@ -31,13 +31,13 @@ typedef struct
 
 typedef struct 
 {
-	float r, g, b;
+	double r, g, b;
 } ColorType;
 
 typedef struct 
 {
 	PointType center;
-	float radius;
+	double radius;
 	ColorType color;
 } SphereType;
 
@@ -49,10 +49,10 @@ typedef struct
 	PointType botRight;
 } ViewingWindowType;
 
-float dotProduct(PointType firstVector, PointType secondVector)
+double dotProduct(PointType firstVector, PointType secondVector)
 {
 	//s = x1 x2 + y1 y2 + z1 z2
-	float results = 
+	double results = 
 		firstVector.x * secondVector.x +
 		firstVector.y * secondVector.y +
 		firstVector.z * secondVector.z;
@@ -81,7 +81,18 @@ PointType addVectors(PointType firstVector, PointType secondVector)
 	return results;
 }
 
-PointType multiplyVector(PointType inputVector, float scalar)
+PointType subtractPoints(PointType firstPoint, PointType secondPoint)
+{
+	//subtract the first from the second.
+	//This finds the vector from firstPoint -> secondPoint
+	PointType results;
+	results.x = secondPoint.x - firstPoint.x;
+	results.y = secondPoint.y - firstPoint.y;
+	results.z = secondPoint.z - firstPoint.z;
+	return results;
+}
+
+PointType multiplyVector(PointType inputVector, double scalar)
 {
 	PointType results;	
 	results.x = inputVector.x * scalar;
@@ -90,7 +101,7 @@ PointType multiplyVector(PointType inputVector, float scalar)
 	return results;
 }
 
-float vectorLength(PointType inputVector)
+double vectorLength(PointType inputVector)
 {
 	//sqrt(x^2 + y^2 + z^2)
 	return sqrt(inputVector.x * inputVector.x
@@ -100,7 +111,7 @@ float vectorLength(PointType inputVector)
 
 PointType normalizeVector(PointType inputVector)
 {
-	float normalizeScalar = 1.0 / vectorLength(inputVector);
+	double normalizeScalar = 1.0 / vectorLength(inputVector);
 	PointType results = multiplyVector(inputVector, normalizeScalar);
 	return results;
 }
@@ -149,41 +160,9 @@ vector<string> tokenizeString(string input)
 	return tokens;
 }
 
-int main( int argc, char *argv[] )
+string getInputFileName(int argc, char *argv[])
 {
-	cout << "Creating a new image.\n";
-
-//Declare all the important variables I'll need
-	
-	//This width and height are in pixels.
-	int width, height;
-	
-	//d, w, h are in world coordinates
-	//d = distance from eye to window.  w=width window, h = height window.
-	float d, w, h;
-	
-	RayType eyeRay;	
-	
-	PointType upVector;
-	PointType u;
-	PointType v;	
-	PointType n;
-	
-	ColorType bgColor;
-	ColorType currentMaterialColor;
-	
-	vector<SphereType> spheres;
-	
-	//The angle of the horizontal field of view.  Theta.
-	float fovH;
-	
-	//contains 4 points for the coordinates of the viewing window.
-	ViewingWindowType viewingWindow;
-
 	string inputFileName;
-	string outputFileName;
-
-//Read commandline args to get the input file name.
 	if ( argc == 2 )
 	{
 		//cout << "arg0:" << argv[0];
@@ -195,19 +174,14 @@ int main( int argc, char *argv[] )
 		cout << "You should have exactly one argument, the input file name.  Using default filename instead.\n";
 		inputFileName = "example_input.txt";
 	}
+	return inputFileName;
+}
 
-	//this is crap but I'm not very good at string operations in c++ right now :D
-	outputFileName = inputFileName.substr(0,inputFileName.length()-4) + ".ppm";
-
-	cout << "Input:" << inputFileName << ":\n";
-	cout << "Output:" << outputFileName << ":\n";
-
-//Read input file to get the scene information.
+void loadSceneInformation(string inputFileName, RayType& eyeRay, PointType& upVector, int& width, int& height, double& fovH
+	, ColorType& bgColor, ColorType& currentMaterialColor, vector<SphereType>& spheres)
+{
 	string line;
-
-	//TODO This should be dynamic for inputFileName passed in.  Instead of text literal.  IDK.  Pointers?
-	//Maybe I need to do a *symbol to dereference something.  I don't know.
-	ifstream myInputfile ("example_input.txt");
+	ifstream myInputfile (inputFileName);
 	if (myInputfile.is_open())
 	{
 		cout << "Input File Found.  Good.\n";
@@ -302,17 +276,12 @@ int main( int argc, char *argv[] )
     	cout << "Unable to open input file. Using default image size values.\n";
 		cout << "width:" << width << "\nheight:" << height <<  "\n";
     }
+}
 
+void setViewingWindow(ViewingWindowType& viewingWindow, RayType eyeRay, PointType upVector, int width, int height, double fovH
+	, double& d, double& w, double& h , PointType& u, PointType& v)
+{
 
-
-
-//Initialize pixel array for output image
-	ColorType pixelArray [width][height]; 
-
-//Perform necessary preliminary calculations.
-	//I think this is where I define the viewing window?
-	//Yeah I need the u and v vectors.  And the corners of the viewing window.
-	
 	//the u vector is the unit vector orthogonal to the viewing direction and the up direction.
 	// u' is the non-normalized u vector.
 	// view_dir x up_dir = u'
@@ -334,7 +303,7 @@ int main( int argc, char *argv[] )
 	h = w * height / width ;
 	
 	//n is the normalized view direction vector
-	n = normalizeVector(eyeRay.direction);
+	PointType n = normalizeVector(eyeRay.direction);
 	
 	//each of these calculations are one point + 3 vectors.
 	//This results in a point (well, 4 points)
@@ -362,35 +331,189 @@ int main( int argc, char *argv[] )
 		, multiplyVector(n, d))
 		, multiplyVector(v, -h/2))
 		, multiplyVector(u, w/2));
+			
+	cout << "Double check the aspect ratios to see that they match." << endl;
+	double pixAspRat = 1.0 * width /height;
+	cout << "Pixel aspect ratio:" << pixAspRat << endl;
+	double worldAspRat = (vectorLength(addVectors(viewingWindow.botLeft, viewingWindow.botRight)))
+		/ (vectorLength(subtractPoints(viewingWindow.botLeft, viewingWindow.topLeft)));
+	cout << "World aspect ratio:" << pixAspRat << endl;
+
+}
+
+double intersectSphere(RayType inputRay, SphereType inputSphere)
+{
+	//return the distance to the intersection.
+	//return -1 if there is no intersection.
+	//Solving the combined sphere and ray equation to find an intersection.
+	//For the quadratic equation
+	//A = (xd^2 + yd^2 + zd^2) = 1;
+	//B = 2*(xd * (x0 - xc) + yd*(y0-yc) + zd*(z0-zc))
+	//C = (x0-xc)^2 + (y0-yc)^2 + (z0-zc)^2 -r^2
+	
+	double A = 1;
+	double B = 2 * (
+		inputRay.direction.x * (inputRay.origin.x - inputSphere.center.x)
+		+ inputRay.direction.y * (inputRay.origin.y - inputSphere.center.y)
+		+ inputRay.direction.z * (inputRay.origin.z - inputSphere.center.z));
+	double C = 
+		pow( inputRay.origin.x - inputSphere.center.x , 2)
+		+ pow( inputRay.origin.y - inputSphere.center.y , 2)
+		+ pow( inputRay.origin.z - inputSphere.center.z , 2)
+		- pow( inputSphere.radius , 2);
 		
+	//calculate the quadratic equation's discriminant, (B^2 - 4AC)
+	double discriminant = pow(B,2) - (4*A*C);
+	if (discriminant > 0)
+	{
+		//cout << "2 solutions found" << endl;
+		//There are 2 solutions.  t is the "time" until the ray hits the object.
+		//We want the minimum positive t
+		double t1 = (-B + sqrt(discriminant))/(2*A);
+		double t2 = (-B - sqrt(discriminant))/(2*A);
+		//t<0 means that the object is behind the eye.  Can't see it.
+		if(t1 < 0)
+		{
+			return (t2<0) ? -1 : t2;
+		}
+		else if(t2<0)
+		{
+			//Can't see t2
+			return t1;
+		}
+		else
+		{
+			//return the minimum of the two positive solutions.  That's the closest intersection.
+			return ( t1<t2 ? t1 : t2);
+		}
+	}
+	else if (discriminant == 0)
+	{
+		//cout << "1 solution found" << endl;
+		//Don't reckon this'll happen much.  Graze the edge of the sphere.  1 solution.
+		//Discriminant cancelled out.
+		double t = (-B)/(2*A);
+		return (t>0) ? t : -1;
+	}
+	else
+	{
+		//discriminant is negative.  No solution.  No intersection.
+		return -1;
+	}
+}
+
+ColorType trace_ray(ViewingWindowType viewingWindow, RayType eyeRay, int x, int y, PointType deltaH, PointType deltaV
+	, ColorType bgColor, vector<SphereType> spheres)
+{
+	//default to background color.  Switch to sphere color if ray intersects it.
+	ColorType results = bgColor;	
+	//This is the mapping to the world coordinate point.
+	//topleft + deltaH*x + deltaV * y
+	PointType worldCoordinatesPoint = addVectors(addVectors(
+		viewingWindow.topLeft
+		,multiplyVector(deltaH, x))
+		,multiplyVector(deltaV, y));
 		
-		
-		
+	RayType rayToTrace;
+	rayToTrace.origin = eyeRay.origin;
+	rayToTrace.direction = normalizeVector(subtractPoints(rayToTrace.origin, worldCoordinatesPoint));
+			
+	//determine intersection points with each sphere.
+	double intersectDistance = -1;
+	for(int i = 0; i < spheres.size(); i++)
+	{
+		double currentIntersectDistance = intersectSphere(rayToTrace, spheres[i]);
+		if(currentIntersectDistance != -1 && 
+			(currentIntersectDistance < intersectDistance || intersectDistance == -1))
+		{
+			cout << "Intersection Found." << endl;
+			cout << spheres[i].color.r << endl;
+			results = spheres[i].color;
+		}	
+	}
+			
+			
+	return results;
+}
+
+int main( int argc, char *argv[] )
+{
+	cout << "Creating a new image.\n";
+
+//Declare all the important variables I'll need
+	
+	//This width and height are in pixels.
+	int width, height;
+	
+	//d, w, h are in world coordinates
+	//d = distance from eye to window.  w=width window, h = height window.
+	double d, w, h;
+	
+	RayType eyeRay;	
+	
+	PointType upVector;
+	PointType u;
+	PointType v;	
+	
+	ColorType bgColor;
+	ColorType currentMaterialColor;
+	
+	vector<SphereType> spheres;
+	
+	//The angle of the horizontal field of view.  Theta.
+	double fovH;
+	
+	//contains 4 points for the coordinates of the viewing window.
+	ViewingWindowType viewingWindow;
+
+	string inputFileName;
+	string outputFileName;
+
+//Read commandline args to get the input file name.
+	inputFileName = getInputFileName(argc, argv);
+
+	outputFileName = inputFileName.substr(0,inputFileName.length()-4) + ".ppm";
+
+	cout << "Input:" << inputFileName << ":\n";
+	cout << "Output:" << outputFileName << ":\n";
+
+//Read input file to get the scene information.
+	loadSceneInformation(inputFileName, eyeRay, upVector, width, height, fovH
+		, bgColor, currentMaterialColor, spheres);
+
+//Initialize pixel array for output image
+	cout << "Initializing Pixel Array" << endl;
+	ColorType* pixelArray =new ColorType[width*height];
+
+//Perform necessary preliminary calculations.
+	cout << "Starting Preliminary Calculations" << endl;
+	setViewingWindow(viewingWindow, eyeRay, upVector, width, height
+		, fovH, d, w, h ,u , v);
 
 
-
-
-
-
+	
 
 //foreach pixel in image array:
+	//call trace_ray() with appropriate parameters
+	//Use value returend by trace_ray to update pixel colors.
+	cout << "Starting the loop through each pixel" << endl;
+	//Passing in deltaH and deltaV so I don't need to calculate it every time.
+	//deltaH and deltaV are VECTORS.  they indicate how far to increment the viewing window for each pixel.
+	//deltaH = (ur - ul)/(pixwidth - 1)
+	PointType deltaH = multiplyVector( subtractPoints(viewingWindow.topLeft, viewingWindow.topRight) 
+		, (1.0/(width - 1)));
+	PointType deltaV = multiplyVector( subtractPoints(viewingWindow.topLeft, viewingWindow.botLeft) 
+		, (1.0/(height - 1)));
+
 	for(int y = 0; y < height; y++)
 	{
 		for(int x = 0; x < width; x++)
 		{
-			ColorType currentPixel;
-			currentPixel.r = 3;
-			currentPixel.r = 4;
-			currentPixel.r = 200;
-			pixelArray [x][y] = currentPixel;
-			//call trace_ray() with appropriate parameters
-			//Use value returend by trace_ray to update pixel colors.
+			ColorType currentPixel = trace_ray(viewingWindow, eyeRay, x, y, deltaH, deltaV, bgColor, spheres);
+			pixelArray [ x + y*width ] = currentPixel;
 		}
 	}
 	
-	
-
-
 
 //Write the data to a image file.
 
@@ -405,16 +528,16 @@ int main( int argc, char *argv[] )
 	myfile << "P3\n";
 	myfile << "# This image was created by Ben Matern from input file " << inputFileName << "\n";
 	myfile << widthString + " " + heightString + "\n";
-	myfile << "255\n";
+	myfile << "1\n";
 
 	//Loop through each pixel in the picture.
 	for(int y = 0; y < height ; y++)
 	{
 		for(int x = 0; x < width ; x++)
 		{
-			string r = to_string(pixelArray[x][y].r) + " ";
-			string b = to_string(pixelArray[x][y].b) + " ";
-			string g = to_string(pixelArray[x][y].g) + " ";
+			string r = to_string(pixelArray[ x + y*width ].r) + " ";
+			string b = to_string(pixelArray[ x + y*width ].b) + " ";
+			string g = to_string(pixelArray[ x + y*width ].g) + " ";
 			myfile << r << g << b;
 		}
 		//We just finished a row of pixels.
